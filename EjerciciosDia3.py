a8e3d40a2cd9a28f25b04f286415bd24d753b87d
Your Name <you@example.com>
2025-02-18 09:00:43 -0600

    ejercicio 2 ejerciciodia2.py

--- EjerciciosDia3.py
+++ EjerciciosDia3.py
@@ -1,8 +1,8 @@
-# write a script that prompts the usar to enter base and height of the triangle
+## write a script that prompts the user to enter base and height of the triangle
 #calculate an area  of this 
 #triangle (area =0.5 x b x h)
 
-base=float(input("ingresa la base de el triangulo: "))
-altura=float(input("ingresa la altura de el tranigulo: "))
-area= 0.5 * base * altura
-print( "la area de el triangulo ees de :",area)
+base = float(input("ingresa la base de el triangulo: "))
+altura = float(input("ingresa la altura de el tranigulo: "))
+area = 0.5 * base * altura
+print ( "la area de el triangulo es de  ", area)
