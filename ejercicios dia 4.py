429e1554408cb824933c556507936173ac9ee170
Your Name <you@example.com>
2025-03-05 10:31:06 -0600

    55

--- ejercicios dia 4.py
+++ ejercicios dia 4.py
@@ -1,1 +1,28 @@
+# ejercicio 1
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 
+# 'Thirty Days Of Python'.
 
+uno = "thirty "
+dos =  "days "
+tres = "of "
+cuatro = "python "
+print (uno,)
+print(dos,)
+print(tres,)
+print(cuatro,)
+orden = uno + "" ""  + dos + "" "" +  tres + "" "" + cuatro
+print (orden)
+
+#ejercicio 2
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+
+cinco = "coding "
+seis =  "for "
+siete = "all "
+
+print (cinco,)
+print(seis,)
+print(siete,)
+
+orden = cinco + "" ""  + seis + "" "" +  siete
+print (orden)
