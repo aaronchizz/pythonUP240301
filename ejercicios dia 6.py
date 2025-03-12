4dd062c42d8665971e2d2776bf2809754e0b3fb5
Your Name <you@example.com>
2025-03-12 10:31:04 -0600

    re

---
+++ ejercicios dia 6.py
@@ -0,0 +1,67 @@
+#Exercises: Level 1
+
+
+
+#ejercicio 1
+# Create an empty tuple
+
+empty_tuple = ()
+empty_tuple = tuple()
+
+#ejercicio 2
+# Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+
+empty_tuple = ('lizbet'  , 'estefania' , 'yoselin' )
+empty_tupler = ("carlos",  'Luis')
+print(empty_tuple)
+print(empty_tupler)
+
+#ejercicio 3
+#Join brothers and sisters tuples and assign it to siblings
+
+tpl = empty_tuple + empty_tupler
+print("mis hermanos son : " , tpl)
+
+#ejercicio 4
+#How many siblings do you have?
+
+empty_tuple = ('lizbet'  , 'estefania' , 'yoselin' )
+empty_tupler = ("carlos",  'Luis')
+tpl = empty_tuple + empty_tupler
+print('Tengo {} hermanos'.format(len(tpl)))
+
+#ejercicio 5
+#Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+
+
+
+#Exercises: Level 2
+
+#ejercicio 1
+#Unpack siblings and parents from family_members
+
+
+#ejercicio 2
+#Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+
+
+#ejercicio 3
+#Change the about food_stuff_tp tuple to a food_stuff_lt list
+
+#ejercicio 4
+#Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+
+#ejercicio 5
+#Slice out the first three items and the last three items from food_staff_lt list
+
+#ejercicio 6
+#Delete the food_staff_tp tuple completely
+
+#ejercicio 7
+#Check if an item exists in tuple:
+
+#Check if 'Estonia' is a nordic country
+
+#Check if 'Iceland' is a nordic country
+
+#nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
