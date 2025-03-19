f191f120196de9d31f735e45297d323a070a078b
Your Name <you@example.com>
2025-03-19 10:40:08 -0600

    hoymiercoles

---
+++ 07_Day/parte 1.py
@@ -0,0 +1,29 @@
+#ejercicio 1
+#Find the length of the set it_companies
+
+ty = {"it_companies"}
+print(len(ty))
+
+#ejercicio 2
+#Add 'Twitter' to it_companies
+
+ty = {"it_companies"}
+ty.add ('twitter')
+print(ty)
+
+#ejercicio 3
+#Insert multiple IT companies at once to the set it_companies
+
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+
+#ejercicio 4
+#Remove one of the companies from the set it_companies
+
+it_companies.remove("Amazon")
+print(it_companies)
+
+#ejercicio 5
+#What is the difference between remove and discard
+
+#si usando remove () el item no se encuentra en el set ,arrojara error
+#en cambio discard no arroja nada
---
+++ 07_Day/parte 2.py
@@ -0,0 +1,37 @@
+
+#ejercicio 1
+# Join A and B
+
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print(B.union(A))
+
+#ejercicio 2
+#Find A intersection B
+
+print(A.intersection(B))
+
+#ejercicio 3
+#Is A subset of B
+
+print(A.issubset(B))
+
+#ejercicio 4
+#Are A and B disjoint sets
+print(A.isdisjoint(B))
+
+#ejercicio 5
+#Join A with B and B with A
+
+print(A.union(B))
+print(B.union(A))
+
+#ejercicio 6
+#What is the symmetric difference between A and B
+
+print(A.symmetric_difference(B) )
+
+#ejercicio 7
+#Delete the sets completel
+
+
