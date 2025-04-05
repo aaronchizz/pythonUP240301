e407d2078bff1514d7ad99ad56dd6c0bf796f697
Your Name <you@example.com>
2025-04-04 23:25:50 -0600

    FINILLL AL FINNN

---
+++ 15_Day/dia15.py
@@ -0,0 +1,36 @@
+#ejercicio 1 
+# Open you python interactive shell and try all the examples covered in this section.
+
+
+#Syntaxerror
+print('si me reprueba lloro')
+
+#NameError
+print(soy una estudiante de la upa)
+
+#IndexError
+edades=[1,2,3,4,5]
+print(edades[10])
+
+#ModuleNotFoundError
+import UPA
+
+#AttributeError
+import range
+range.add
+
+#KeyError
+estudiante={'apellido':'anya','año de nacimiento':2000}
+print(estudiante['nombe'])
+
+#TypeError
+print('mama' + 5)
+
+#ValueError
+print(int('hije'))
+
+#ImportError
+import math
+import math1
+
+
